12560f6f70eff29854aad82b64c55ff00c9b3306
gfathertech <gfathertech@gmail.com>
2025-12-25 19:11:52 +0100

    Update Mongodb.js

--- src/Core/Mongodb.js
+++ src/Core/Mongodb.js
@@ -1,5 +1,5 @@
 import { MongoClient } from 'mongodb'
-import { proto } from 'baileys'
+import { initAuthCreds } from 'baileys'
 
 const MONGO_URI = process.env.MONGO_URI
 const DB_NAME = 'whatsapp'
@@ -11,11 +11,15 @@ let authCollection
 async function initMongo() {
   if (authCollection) return authCollection
 
-  mongoClient = new MongoClient(MONGO_URI)
-  await mongoClient.connect()
+  mongoClient = new MongoClient(MONGO_URI, {
+    tls: true,
+    serverSelectionTimeoutMS: 10000
+  })
 
+  await mongoClient.connect()
   const db = mongoClient.db(DB_NAME)
   authCollection = db.collection(COLLECTION)
+
   return authCollection
 }
 
@@ -23,32 +27,33 @@ export async function useMongoAuthState() {
   const col = await initMongo()
 
   const credsDoc = await col.findOne({ _id: 'creds' })
-
-  const creds = credsDoc?.data
-    ? proto.Credential.fromObject(credsDoc.data)
-    : proto.Credential.fromObject({})
+  const creds = credsDoc?.data ?? initAuthCreds()
 
   const keys = {
     get: async (type, ids) => {
-      const data = await col
+      const docs = await col
         .find({ _id: { $in: ids.map(id => `${type}-${id}`) } })
         .toArray()
 
       return Object.fromEntries(
-        data.map(doc => [doc._id.replace(`${type}-`, ''), doc.data])
+        docs.map(d => [d._id.replace(`${type}-`, ''), d.data])
       )
     },
 
     set: async data => {
       const bulk = col.initializeUnorderedBulkOp()
+      let hasOps = false
+
       for (const category in data) {
         for (const id in data[category]) {
+          hasOps = true
           bulk.find({ _id: `${category}-${id}` }).upsert().updateOne({
             $set: { data: data[category][id] }
           })
         }
       }
-      if (bulk.length > 0) await bulk.execute()
+
+      if (hasOps) await bulk.execute()
     }
   }
 
