b322f1992b7133fadb47f84966124bb8a4673341
gfathertech <gfathertech@gmail.com>
2025-12-25 18:06:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const prefix = ['.'] // prefix bot
 const botName = 'Kkzaabot' // bot name
 const owner = ['447777331420', '2349136429929'] // owner numbers (without @s.whatsapp.net)
 const botMode = 'group' // 'group' or 'private' or 'both'
-const usePairing = false // true or false
+const usePairing = true // true or false
 
 export const config = {
 	botName,
