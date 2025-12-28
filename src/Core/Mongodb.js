4d98c554ed54aefa843eb5cec08355957f359dd3
gfathertech <gfathertech@gmail.com>
2025-12-28 11:42:45 +0100

    Update Mongodb.js

--- src/Core/Mongodb.js
+++ src/Core/Mongodb.js
@@ -1,5 +1,5 @@
 import { MongoClient } from 'mongodb'
-import { initAuthCreds } from 'baileys'
+import { initAuthCreds, BufferJSON } from 'baileys'
 
 const MONGO_URI = process.env.MONGO_URI
 const DB_NAME = 'whatsapp'
@@ -8,63 +8,73 @@ const COLLECTION = 'auth'
 let mongoClient
 let authCollection
 
-async function initMongo() {
-  if (authCollection) return authCollection
+export async function initMongo() {
+    if (authCollection) return authCollection
 
-  mongoClient = new MongoClient(MONGO_URI, {
-    tls: true,
-    serverSelectionTimeoutMS: 10000
-  })
+    mongoClient = new MongoClient(MONGO_URI, {
+        tls: true,
+        serverSelectionTimeoutMS: 10000
+    })
 
-  await mongoClient.connect()
-  const db = mongoClient.db(DB_NAME)
-  authCollection = db.collection(COLLECTION)
+    await mongoClient.connect()
+    const db = mongoClient.db(DB_NAME)
+    authCollection = db.collection(COLLECTION)
 
-  return authCollection
+    return authCollection
 }
 
 export async function useMongoAuthState() {
-  const col = await initMongo()
+    const col = await initMongo()
 
-  const credsDoc = await col.findOne({ _id: 'creds' })
-  const creds = credsDoc?.data ?? initAuthCreds()
+    const credsDoc = await col.findOne({ _id: 'creds' })
+    
+    // CHANGE 1: Use BufferJSON.reviver to restore specific Baileys object types
+    const creds = credsDoc 
+        ? JSON.parse(credsDoc.value, BufferJSON.reviver) 
+        : initAuthCreds()
 
-  const keys = {
-    get: async (type, ids) => {
-      const docs = await col
-        .find({ _id: { $in: ids.map(id => `${type}-${id}`) } })
-        .toArray()
+    const keys = {
+        get: async (type, ids) => {
+            const data = {}
+            // CHANGE 2: Kept the $in fix from before
+            const docs = await col
+                .find({ _id: { $in: ids.map(id => `${type}-${id}`) } })
+                .toArray()
 
-      return Object.fromEntries(
-        docs.map(d => [d._id.replace(`${type}-`, ''), d.data])
-      )
-    },
+            docs.forEach(doc => {
+                // CHANGE 3: Parse properly using reviver
+                data[doc._id.replace(`${type}-`, '')] = JSON.parse(doc.value, BufferJSON.reviver)
+            })
 
-    set: async data => {
-      const bulk = col.initializeUnorderedBulkOp()
-      let hasOps = false
+            return data
+        },
 
-      for (const category in data) {
-        for (const id in data[category]) {
-          hasOps = true
-          bulk.find({ _id: `${category}-${id}` }).upsert().updateOne({
-            $set: { data: data[category][id] }
-          })
+        set: async (data) => {
+            const bulk = col.initializeUnorderedBulkOp()
+            let hasOps = false
+
+            for (const category in data) {
+                for (const id in data[category]) {
+                    hasOps = true
+                    // CHANGE 4: Store as stringified JSON to prevent BSON corruption
+                    bulk.find({ _id: `${category}-${id}` }).upsert().updateOne({
+                        $set: { value: JSON.stringify(data[category][id], BufferJSON.replacer) }
+                    })
+                }
+            }
+
+            if (hasOps) await bulk.execute()
         }
-      }
-
-      if (hasOps) await bulk.execute()
     }
-  }
 
-  return {
-    state: { creds, keys },
-    saveCreds: async () => {
-      await col.updateOne(
-        { _id: 'creds' },
-        { $set: { data: creds } },
-        { upsert: true }
-      )
+    return {
+        state: { creds, keys },
+        saveCreds: async () => {
+            await col.updateOne(
+                { _id: 'creds' },
+                { $set: { value: JSON.stringify(creds, BufferJSON.replacer) } },
+                { upsert: true }
+            )
+        }
     }
-  }
 }
